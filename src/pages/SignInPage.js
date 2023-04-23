bb14a3b0fdbf808f8aa59d2efda371e8450a0b4b
devmatheusdias <dev.matheusdias@gmail.com>
2023-04-23 13:13:50 -0300

    signUp and signIn

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,18 +1,36 @@
+import axios from 'axios';
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
 
 export default function SignInPage() {
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  function signIn(e){
+
+    e.preventDefault();
+
+    axios.post("http://localhost:5000/", {email, password})
+    .then(res => navigate("/home"))    
+    .catch(err => alert('insira os dados corretamente'))
+  }
+
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signIn}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input placeholder="E-mail" type="email" value={email} onChange={e => setEmail(e.target.value)} required/>
+        <input placeholder="Senha" type="password" autocomplete="new-password" value={password} onChange={e => setPassword(e.target.value)} required/>
+        <button type='submit'>Entrar</button>
       </form>
 
-      <Link>
+      <Link to={'/cadastro'}>
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,17 +1,51 @@
-import { Link } from "react-router-dom"
+import axios from 'axios';
+import { Link, Navigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
+
 
 export default function SignUpPage() {
+
+  const navigate = useNavigate();
+
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  function signUp(e) {
+
+    e.preventDefault();
+
+    if (passwordConfirm !== password) {
+      setPasswordConfirm("");
+      return alert("insira um password valido!")
+    }
+
+    axios.post("http://localhost:5000/cadastro", {name, email, password})
+      .then(res => navigate("/"))
+      .catch(err => alert('insira os dados corretamente'));
+
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input placeholder="Nome" type="text" value={name} onChange={e => setName(e.target.value)} required />
+
+        <input placeholder="E-mail" type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+
+        <input placeholder="Senha" type="password" autocomplete="new-password" value={password} onChange={e => setPassword(e.target.value)} minLength={3} required />
+
+        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" value={passwordConfirm} onChange={e => setPasswordConfirm(e.target.value)} minLength={3} required />
+
+        <button type="submit">Cadastrar</button>
+
       </form>
 
       <Link>
